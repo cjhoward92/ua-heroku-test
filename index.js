82d52a5b9178c7eb151e7852b3541c9410f0d69a
Carson Howard <cjhoward92@gmail.com>
2018-10-22 17:39:29 -0700

    Add more complex shit

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 var http = require('http');
+var request = require('request');
 
 var server = http.createServer(function(req, res) {
-  res.end('Hello, world!');
+  request('http://github.com/cjhoward92/ua-heroku-test', function (err, response, body) {
+    if (err) {
+      res.writeHead(500, err.message);
+      res.end(err.message);
+      return;
+    }
+
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(body);
+  });
 });
 
 var port = process.env.PORT || 80;
